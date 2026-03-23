86fbf6c546f289ac641c4975b0730304e36e5234
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-23 08:25:15 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/custom_code/actions/rfid_connection.dart
@@ -0,0 +1,16 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom actions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+//
+Future<bool> rfidConnection() async {
+  await Future.delayed(Duration(seconds: 2));
+  return true;
+}
+
+// Set your action name, define your arguments and return parameter,
+// and then add the boilerplate code using the green button on the right!
---
+++ lib/pages/home_page/home_page_model.dart
@@ -0,0 +1,20 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'home_page_widget.dart' show HomePageWidget;
+import 'package:flutter/material.dart';
+
+class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  ///  Local state fields for this page.
+
+  String? connectionStatus;
+
+  ///  State fields for stateful widgets in this page.
+
+  // Stores action output result for [Custom Action - rfidConnection] action in Button widget.
+  bool? rfidConnectionStatus;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
